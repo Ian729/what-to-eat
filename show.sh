e611ed8f2cf2d142ef7e05fe31ecf2d99e9882a0
Ian <weixiang_zheng@apple.com>
2022-10-18 18:04:56 +0800

    update

--- show.sh
+++ show.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-
-# 找到相對應的目錄
-cd $HOME/Desktop/what-to-eat
-# 執行 main.js 取得隨機店家，並 assign 給 lunch
-lunch=`/usr/local/bin/node main.js`
-# 跑 Mac 內建的 script，製造推播推知
-osascript -e "display notification \"$lunch\" with title \"今天午餐吃什麼 ?\""
+#!/bin/sh
+lunch=`python3 /Users/weixiang/what-to-eat/main.py`
+osascript -e "display notification \"$lunch\" with title \"今天吃什么?\""
 # 我叫 Mac 唸給妳聽！
-say $lunch
+# say $lunch
