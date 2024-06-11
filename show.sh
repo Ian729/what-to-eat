1136309da35ac5a42786dbee9f3686b0f22ffbfa
Ian <weixiang_zheng@apple.com>
2024-06-11 20:11:29 +0800

    feat: use Gaode API to find nearby restaurants
    Signed-off-by: Ian <weixiang_zheng@apple.com>

--- show.sh
+++ show.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-lunch=`python3 /Users/weixiang/what-to-eat/main.py`
+# lunch=`python3 /Users/weixiang/what-to-eat/main.py`
+lunch=`python ./gaode.py`
 osascript -e "display notification \"$lunch\" with title \"今天吃什么?\""
 # 我叫 Mac 唸給妳聽！
 # say $lunch
